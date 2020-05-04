a7ab32aed6f41c85b24a90fbdb24fc07b29a4be0
hyc <741096681@qq.com>
2020-05-04 22:15:30 +0800

    添加日志

--- src/main/java/com/community/community/controller/AuthorizeController.java
+++ src/main/java/com/community/community/controller/AuthorizeController.java
@@ -6,6 +6,7 @@ import com.community.community.dto.GithubUser;
 import com.community.community.mapper.UserMapper;
 import com.community.community.model.User;
 import com.community.community.model.UserExample;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController  {
 
 	@Autowired
@@ -71,6 +73,7 @@ public class AuthorizeController  {
 
 			return "redirect:/";
 		} else {
+			log.error("callback get github user error", githubUser);
 			// 登录失败，重新登录
 			return "redirect:/";
 		}
